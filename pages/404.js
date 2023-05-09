5c127d94b28145f9b0ac8f1a8f0c69bdca637017
fjordservers <128976682+fjordservers@users.noreply.github.com>
2023-05-09 18:25:24 +0400

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,8 @@
 // 404.js
 import Link from 'next/link'
 
+import styles from '../styles/404.module.css';
+
 export default function FourOhFour() {
   return <>
     <h1>404 - Sorry But The Page Was Not Found</h1>
